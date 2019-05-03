ad45f4bee13c32c2c4e527d292f2a610db0e966e
practicalgnome <practicalgnome@gmail.com>
2019-05-03 15:27:51 +0300

    init dev

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import Auxiliary from '../../hoc/Auxiliary';
+import PropTypes from 'prop-types';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Auxiliary>
+
+            </Auxiliary>
+        );
+    }
+}
+
+BurgerBuilder.propTypes = {};
+
+export default BurgerBuilder;
